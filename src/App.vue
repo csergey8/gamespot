f28349a76383b1061a36185a965b42928e62dc24
csergey8 <c.sergey@protonmail.com>
2019-04-15 10:59:35 +0300

    template and header and footer

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <comp-header />
+    <div>content</div>
+    <comp-footer />
+  </div>
+</template>
+
+<script>
+import compHeader from './components/Header/index.vue';
+import compFooter from './components/Footer/index.vue';
+
+export default {
+  components: {
+    compHeader,
+    compFooter
+  }
+  
+}
+</script>
+
+<style>
+  @import './assets/style.css';
+</style>
