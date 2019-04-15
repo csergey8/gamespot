c836cc8fccea6c04a40fb7fd2645ff106247623e
csergey8 <c.sergey@protonmail.com>
2019-04-15 14:12:15 +0300

    add routes logic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './routes';
+
+import Button from './components/UI/button.vue';
+
+Vue.component('app-button', Button);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
