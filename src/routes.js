a57ec8bda2b2bf0d601cc8048b3bcef4fbce5c70
csergey8 <c.sergey@protonmail.com>
2019-04-16 15:42:17 +0300

    logout functionality and route guard

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from './components/Store/store';
 
 import Home from './components/Home/index.vue';
 import Signin from './components/Signin/index.vue';
+import Dashboard from './components/Dashboard/index.vue';
 
 Vue.use(VueRouter);
 
 
+const authGuard = {
+  beforeEnter: (to, from, next) => {
+    if(store.state.admin.token) {
+      next()
+    } else {
+      next('/')
+    }
+  }
+}
+
 const routes = [
   { path: '/', component: Home},
   { path: '/signin', component: Signin},
+  { path: '/dashboard', component: Dashboard, children: [], ...authGuard},
 ]
 
 export default new VueRouter({
