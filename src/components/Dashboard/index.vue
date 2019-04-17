1a59322527d42562c0faf50131f52f4610f7723a
csergey8 <c.sergey@protonmail.com>
2019-04-17 13:43:36 +0300

    added dashboard nested routes

--- src/components/Dashboard/index.vue
+++ src/components/Dashboard/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-   dashboard 
+  <div class="container dashboard_container">
+   <div class="admin_nav">
+     <ul>
+       <li>
+         <router-link to="/dashboard/add_posts">Add posts</router-link>
+       </li>
+       <li>
+         <router-link to="/dashboard/list_posts">Manage posts</router-link>
+       </li>
+     </ul>
+   </div>
+   <router-view />
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
