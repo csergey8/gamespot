8e030a31d789bc1a86bee031eb3213c2afb3c064
csergey8 <c.sergey@protonmail.com>
2019-04-19 17:32:31 +0300

    load more posts functionality

--- src/components/Home/content.vue
+++ src/components/Home/content.vue
@@ -23,16 +23,36 @@
         </md-card-actions>
       </md-card>
     </div>
+    <div class="load_more">
+      <app-button 
+        type="btn"
+        :addClass="['small_link']"
+        :action="loadMore"
+      ><div @click="loadMore">Load more</div></app-button>
+    </div>
+
   </div>
 </template>
 
 <script>
 //import posts from '../../assets/posts.js';
 export default {
+  data() {
+    return {
+      info1: 1
+    }
+  },
   created() {
     this.$store.dispatch('posts/getAllPosts', {
       limit: 3
     })
+  },
+  methods: {
+      loadMore() {
+        this.$store.dispatch('posts/getAllPosts', {
+          limit: this.posts.length + 3
+         })
+      }
   },
   computed: {
     posts() {
